f6e672caf5908daa244e5beb3241a4c2b3206945
amitutkarsh06 <134827262+amitutkarsh06@users.noreply.github.com>
2024-08-26 11:26:20 +0530

    1-user controllers done

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,32 @@
 import express from "express";
 import dotnev from "dotenv";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/userRoutes.js";
+
+
 import connectMongoDB from "./db/mongoDB.connect.js";
-import cookieParser from "cookie-parser";
 
 dotnev.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 const app = express();
 const PORT = process.env.PORT | 5000;
 
 app.use(express.json()); //to parse request body 
 app.use(express.urlencoded({ extended: true }));
+
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.uue("/api/users", userRoutes);
 
 
 app.listen(PORT, () => {
